ced2cf33c029508139c48cb000228026c5639423
Abram <abramwpierce@icloud.com>
2023-01-12 16:36:36 -0700

    implemented the field as a background

--- bezier.py
+++ bezier.py
@@ -9,33 +9,36 @@ pygame.init()
 running = True
 clicked = False
 pressed = False
-points = [(14, 50), (40, 20)]
+points = []
 slopes = []
 lengths = []
 lerp1 = []
 animate = False
 neighbors = [(int(count / 5) - 2, (count % 5) - 2) for count in range(0, 25)]
 
+image = pygame.image.load('2023-FRC-Field.png')
+size = image.get_size()
+image = pygame.transform.scale(image, (size[0] * 1.8, size[1] * 1.8))
+
 step = 0
 direction = 1
 clock = pygame.time.Clock()
 
-window = pygame.display.set_mode((900, 600))
+window = pygame.display.set_mode(((size[0] * 1.8) + 140, (size[1] * 1.8) + 140))
 
 def draw_lines():
 	pygame.draw.lines(window, (220, 220, 220), False, points, 3)
+
+def draw_circles():
 	for point in points:
 		pygame.draw.circle(window, (220, 220, 220), point, 7, 3)
-	if animate:
-		for lerp in lerp1:
-			pygame.draw.circle(window, (220, 220, 220), lerp, 5, 2)
 
 def draw_curve():
 	points2 = a(points)
 	t_points = np.arange(0, 1, 0.01)
 	curve = Bezier.Curve(t_points, points2)
 	for c in range(len(curve) - 1):
-		pygame.draw.lines(window, (20, 20, 220), False, [curve[c], curve[c + 1]], 2)
+		pygame.draw.lines(window, (20, 20, 220), False, [curve[c], curve[c + 1]], 5)
 	
 while running:
 	for event in pygame.event.get():
@@ -58,12 +61,13 @@ while running:
 			if clicked_index == 0:
 				points.append(mouse_pos)
 
-			slopes = [i for i in range(len(points) - 1)]
-			lengths = [i for i in range(len(points) - 1)]
-			lerp1 = [list(point) for point in points[0:-1]]
-			for point in range(len(points) - 1):
-				slopes[point] = abs(points[point][1] - points[point + 1][1]) / abs(points[point][0] - points[point + 1][0])
-				lengths[point] = math.sqrt((points[point][1] - points[point + 1][1])**2 + (points[point][0] - points[point + 1][0])**2)
+			if len(points) > 1:
+				slopes = [i for i in range(len(points) - 1)]
+				lengths = [i for i in range(len(points) - 1)]
+
+				for point in range(len(points) - 1):
+					slopes[point] = abs(points[point][1] - points[point + 1][1]) / abs(points[point][0] - points[point + 1][0])
+					lengths[point] = math.sqrt((points[point][1] - points[point + 1][1])**2 + (points[point][0] - points[point + 1][0])**2)
 	else:
 		clicked = False
 		clicked_index = 0
@@ -76,8 +80,12 @@ while running:
 
 
 	window.fill((30, 50, 60))
-	draw_lines()
-	draw_curve()
+	window.blit(image, (70, 70))
+	if len(points) > 0:
+		draw_circles()
+	if len(points) > 1:
+		draw_lines()
+		draw_curve()
 	pygame.display.update()
 
 
